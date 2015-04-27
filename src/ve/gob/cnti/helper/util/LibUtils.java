394746647dda29036d3c4460bb2f8f7f86e1983f
alexis veliz <aveliz@cnti.gob.ve>
2015-04-27 15:10:26 -0430

    Agregando el la clase LibUtils el tipo de entrada List<String> y modificando la clase GenerateBean los siguiente metodos createAttribute, createMethodSelectValue, createSetMethod, createGetMethod

--- src/ve/gob/cnti/helper/util/LibUtils.java
+++ src/ve/gob/cnti/helper/util/LibUtils.java
@@ -19,18 +19,18 @@ public class LibUtils {
 
 		return Character.toUpperCase(word.charAt(0)) + word.substring(1);
 	}
-	
+
 	public static String firstLetterLower(String word) {
 
 		return Character.toLowerCase(word.charAt(0)) + word.substring(1);
 	}
-	
-	public static String readSnippetFile(String pathFile){
+
+	public static String readSnippetFile(String pathFile) {
 		ReadFile rf = new ReadFile();
-		
+
 		String textFile = rf.readFile(pathFile);
-		
-		return textFile;		
+
+		return textFile;
 	}
 
 	public static boolean createDirs(String path) {
@@ -40,66 +40,68 @@ public class LibUtils {
 		return dir.mkdirs();
 
 	}
-	
-	public static String convertPackage2Dirs(String packages){
-		
+
+	public static String convertPackage2Dirs(String packages) {
+
 		return packages.replaceAll("\\.", "/");
 	}
-	
-	public static boolean cretePackagesDirs(String packages){
-		
+
+	public static boolean cretePackagesDirs(String packages) {
+
 		return createDirs(packages);
 	}
-	
+
 	public static String replacePattern(String pattern, String replacement, String text) {
 
 		return text.replaceAll(pattern, replacement);
 	}
-	
-	public static Properties loadFileProperties(String file){
-		
+
+	public static Properties loadFileProperties(String file) {
+
 		InputStream input = null;
 		Properties props = new Properties();
-		
+
 		try {
 			input = new FileInputStream(file);
 			props.load(input);
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}catch (IOException e) {
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return props;
 	}
-	
-	public static String changeToPrimitiveType(String objectType){
-		
+
+	public static String changeToPrimitiveType(String objectType) {
+
 		String primitiveType = null;
-		
-		if("Byte".equals(objectType)){
+
+		if ("Byte".equals(objectType)) {
 			primitiveType = "byte";
-		}else if("Short".equals(objectType)){
+		} else if ("Short".equals(objectType)) {
 			primitiveType = "short";
-		}else if ("Integer".equals(objectType)){
+		} else if ("List".equals(objectType)) {
+			primitiveType = "List<String>";
+		} else if ("Integer".equals(objectType)) {
 			primitiveType = "int";
-		}else if ("Long".equals(objectType)){
+		} else if ("Long".equals(objectType)) {
 			primitiveType = "long";
-		}else if ("Float".equals(objectType)){
+		} else if ("Float".equals(objectType)) {
 			primitiveType = "float";
-		}else if ("Double".equals(objectType)){
+		} else if ("Double".equals(objectType)) {
 			primitiveType = "double";
-		}else if ("Character".equals(objectType)){
+		} else if ("Character".equals(objectType)) {
 			primitiveType = "char";
-		}else if ("Boolean".equals(objectType)){
+		} else if ("Boolean".equals(objectType)) {
 			primitiveType = "boolean";
-		}else{
+		} else {
 			primitiveType = objectType;
 		}
-		
+
 		return primitiveType;
 	}
-	
+
 }
--- src/ve/gob/cnti/output/bean/GenerateBean.java
+++ src/ve/gob/cnti/output/bean/GenerateBean.java
@@ -1,4 +1,9 @@
 package ve.gob.cnti.output.bean;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
 import ve.gob.cnti.helper.form.Field;
 import ve.gob.cnti.helper.output.WriteFile;
 import ve.gob.cnti.helper.util.LibUtils;
@@ -9,23 +14,27 @@ public class GenerateBean {
 	private String origSnippetFile = null;
 	private WriteFile wf = null;
 	private String pathOutputFile = null;
-	
+
 	private String imports = null;
 	private String attributes = null;
+	private String values = null;
 	private String setAndGetMethods = null;
 	private String nameApp = null;
-	
 
 	public GenerateBean(String pathFile, String pathOutputFile, String nameApp) {
-		
+
 		this.origSnippetFile = LibUtils.readSnippetFile(pathFile);
 		this.pathOutputFile = pathOutputFile;
 		this.wf = new WriteFile();
 		this.nameApp = nameApp;
 	}
-	
+
 	public void setImports(String imports) {
 		this.imports = imports;
+	}
+
+	public void setValues(String values) {
+		this.values = values;
 	}
 
 	public void setAttributes(String attributes) {
@@ -37,59 +46,58 @@ public class GenerateBean {
 	}
 
 	public void replaceNameBeanAndNameClassBean(String nameBean) {
-		
-		//Para colocar la variable con el snippet original en la siguiente iteración
+
+		// Para colocar la variable con el snippet original en la siguiente iteración
 		this.snippetFile = this.origSnippetFile;
 
 		if (!Character.isLowerCase(nameBean.charAt(0))) {
-			this.snippetFile = LibUtils.replacePattern("%\\{nameBean\\}",this.nameApp+"_"+LibUtils.firstLetterLower(nameBean), this.snippetFile);
-			this.snippetFile = LibUtils.replacePattern("%\\{NameBean\\}", nameBean,this.snippetFile);
+			this.snippetFile = LibUtils.replacePattern("%\\{nameBean\\}", this.nameApp + "_" + LibUtils.firstLetterLower(nameBean), this.snippetFile);
+			this.snippetFile = LibUtils.replacePattern("%\\{NameBean\\}", nameBean, this.snippetFile);
 		} else {
-			this.snippetFile = LibUtils.replacePattern("%\\{nameBean\\}", this.nameApp+"_"+nameBean,this.snippetFile);
+			this.snippetFile = LibUtils.replacePattern("%\\{nameBean\\}", this.nameApp + "_" + nameBean, this.snippetFile);
 			nameBean = LibUtils.firstLetterUpper(nameBean);
-			this.snippetFile = LibUtils.replacePattern("%\\{NameBean\\}", nameBean,this.snippetFile);
+			this.snippetFile = LibUtils.replacePattern("%\\{NameBean\\}", nameBean, this.snippetFile);
 		}
 	}
-	
-	public void replacePackagesNameBean(String packagesName) {
-		this.snippetFile = LibUtils.replacePattern("%\\{packages\\}", packagesName,this.snippetFile);
-	}
-	
-	public void createImpAttAndMethos(Field field){
-		
-		if (!"".equals(field.getReturnType())){
 
-			if (!field.getReturnType().matches("java\\.lang\\..*") && !this.imports.contains(field.getReturnType())){
+	public void replacePackagesNameBean(String packagesName) {
+		this.snippetFile = LibUtils.replacePattern("%\\{packages\\}", packagesName, this.snippetFile);
+	}
+
+	public void createImpAttAndMethos(Field field) {
+
+		if (!"".equals(field.getReturnType())) {
+
+			if (!field.getReturnType().matches("java\\.lang\\..*") && !this.imports.contains(field.getReturnType())) {
 				this.imports += createImports(field.getReturnType());
 			}
-			
+
 			String nameField = field.getVarName();
-			String returnTypes = field.getReturnType().substring(field.getReturnType().lastIndexOf(".")+1);
-				
-			this.attributes += createAttribute(nameField, returnTypes,field.isActuation());
-			this.setAndGetMethods += createSetMethod(nameField, returnTypes)+"\n";
-			this.setAndGetMethods += createGetMethod(nameField, returnTypes)+"\n";
+			String typeField = field.getTypeField();
+			List<String> nameOptions = field.getOptionValue();
+			String returnTypes = field.getReturnType().substring(field.getReturnType().lastIndexOf(".") + 1);
+
+			this.attributes += createAttribute(nameField, returnTypes, typeField, field.isActuation(), nameOptions);
+			this.values += createMethodSelectValue(nameOptions) + "\n";
+			this.setAndGetMethods += createSetMethod(nameField, returnTypes, typeField) + "\n";
+			this.setAndGetMethods += createGetMethod(nameField, returnTypes, typeField) + "\n";
 		}
 
 	}
-	
-	public void replaceVaraiablesAndWriteFile(String nameBean){
-		
-		this.snippetFile = LibUtils.replacePattern("%\\{imports\\}",this.imports,snippetFile);
-		this.snippetFile = LibUtils.replacePattern("%\\{attributes\\}",this.attributes,snippetFile);
-		this.snippetFile = LibUtils.replacePattern("%\\{setAndGetMethods\\}",this.setAndGetMethods,snippetFile);
-		
-		this.wf.writeFile(this.pathOutputFile+"/"+nameBean+".java", this.snippetFile);
+
+	public void replaceVaraiablesAndWriteFile(String nameBean) {
+
+		this.snippetFile = LibUtils.replacePattern("%\\{imports\\}", this.imports, snippetFile);
+		this.snippetFile = LibUtils.replacePattern("%\\{attributes\\}", this.attributes, snippetFile);
+		this.snippetFile = LibUtils.replacePattern("%\\{values\\}", this.values, snippetFile);
+		this.snippetFile = LibUtils.replacePattern("%\\{setAndGetMethods\\}", this.setAndGetMethods, snippetFile);
+
+		this.wf.writeFile(this.pathOutputFile + "/" + nameBean + ".java", this.snippetFile);
 	}
 
-	
-	//Convierte el nombre del paquetre definido en el configuracion.properties en una ruta de directorio valida
+	// Convierte el nombre del paquetre definido en el configuracion.properties en una ruta de directorio valida
 	/*
-	 * Ejemplo:
-	 * entrada
-	 * ve.gob.vicepresidencia.jubilacionespecial.model
-	 * salida
-	 * /ve/gob/vicepresidencia/jubilacionespecial/model
+	 * Ejemplo: entrada ve.gob.vicepresidencia.jubilacionespecial.model salida /ve/gob/vicepresidencia/jubilacionespecial/model
 	 */
 	public boolean createPackageDirsBean(String packages) {
 		String pathPackage = LibUtils.convertPackage2Dirs(packages);
@@ -101,32 +109,76 @@ public class GenerateBean {
 		return "import " + imports + ";\n";
 	}
 
-	private String createAttribute(String name, String type, boolean isReadOnly) {
-		if(isReadOnly){
-			return "@FieldIsActuacion\nprivate " + LibUtils.changeToPrimitiveType(type) + " " + name + ";\n";
-		}else{
-			return "private " + LibUtils.changeToPrimitiveType(type) + " " + name + ";\n";
+	private String createAttribute(String name, String type, String typeInput, boolean isReadOnly, List<String> options) {
+		if (isReadOnly) {
+			if (type.equals("List") && !name.matches("^_FILE_.*$")) {
+				return "private " + "String[]" + " " + name + ";\n" + "@FieldIsActuacion\nprivate " + "List<String>" + " option" + name + " = Arrays.asList(" + this.createMethodSelectValue(options) + ");\n";
+			} else if (typeInput.equals("LISTBOX_SIMPLE") || typeInput.equals("RADIOBUTTON_GROUP")) {
+				return "private " + "String" + " " + name + ";\n" + "@FieldIsActuacion\nprivate " + "List<String>" + " option" + name + " = Arrays.asList(" + this.createMethodSelectValue(options) + ");\n";
+			} else {
+				return "@FieldIsActuacion\nprivate " + LibUtils.changeToPrimitiveType(type) + " " + name + ";\n";
+			}
+		} else {
+			if (type.equals(LibUtils.changeToPrimitiveType("List<String>")) && !name.matches("^_FILE_.*$")) {
+				return "private " + "String[]" + " " + name + ";\n";
+			} else {
+				return "private " + LibUtils.changeToPrimitiveType(type) + " " + name + ";\n";
+			}
 		}
-			
+
+	}
+
+	public String createMethodSelectValue(List<String> nameOptions) {
+
+		String option = "";
+		for (int k = 0; k < nameOptions.size(); k++) {
+			option += "\"" + nameOptions.get(k) + "\"";
+			if (k < (nameOptions.size() - 1)) {
+				option += ",";
+			}
+
+		}
+		return option;
+
+	}
+
+	/*
+	 * "LISTBOX_SIMPLE"
+	 * 
+	 * "LISTBOX_MULTIPLE" RADIOBUTTON_GROUP
+	 */
+	public String createSetMethod(String name, String type, String typeInput) {
+
+		String method =null;
 		
-	}
-
-	public String createSetMethod(String name, String type) {
-
-		String method = "public void set" + LibUtils.firstLetterUpper(name)
-				+ "(" + LibUtils.changeToPrimitiveType(type) + " " + name + " ) {\n" + "\t this." + name
-				+ " = " + name + ";\n" + "}\n";
-
+		if (type.equals("List") && !name.matches("^_FILE_.*$")) {
+			method="public void set" + LibUtils.firstLetterUpper(name) + "(" + "String[]" + " " + name + " ) {\n" + "\t this." + name + " = " + name + ";\n" + "}\n";
+		    method+= "public void setOption" + LibUtils.firstLetterUpper(name) + "(" + "List<String>" + " option" + name + " ) {\n" + "\t this.option" + name + " = option" + name + ";\n" + "}\n";
+		} else if (typeInput.equals("LISTBOX_SIMPLE") || typeInput.equals("RADIOBUTTON_GROUP")) {
+			method="public void set" + LibUtils.firstLetterUpper(name) + "(" + "String" + " " + name + " ) {\n" + "\t this." + name + " = " + name + ";\n" + "}\n";
+		    method+= "public void setOption" + LibUtils.firstLetterUpper(name) + "(" + "List<String>" + " option" + name + " ) {\n" + "\t this.option" + name + " = option" + name + ";\n" + "}\n";
+		} else {
+			method = "public void set" + LibUtils.firstLetterUpper(name) + "(" + LibUtils.changeToPrimitiveType(type) + " " + name + " ) {\n" + "\t this." + name + " = " + name + ";\n" + "}\n";
+		}	
+		
 		return method;
 	}
 
-	public String createGetMethod(String name, String type) {
+	public String createGetMethod(String name, String type, String typeInput) {
 
-		String method = "public " + LibUtils.changeToPrimitiveType(type) + " get"
-				+ LibUtils.firstLetterUpper(name) + "() {\n"
-				+ "\t return this." + name + ";\n" + "}\n";
-
+		String method = null;
+		if (type.equals("List") && !name.matches("^_FILE_.*$")) {
+			method="public " + "String[]" + " get" + LibUtils.firstLetterUpper(name) + "() {\n" + "\t return this." + name + ";\n" + "}\n";
+		    method+= "public " + "List<String>" + " getOption" + LibUtils.firstLetterUpper(name) + "() {\n" + "\t return this.option" + name + ";\n" + "}\n";
+		} else if (typeInput.equals("LISTBOX_SIMPLE") || typeInput.equals("RADIOBUTTON_GROUP")) {
+			method="public " + "String" + " get" + LibUtils.firstLetterUpper(name) + "() {\n" + "\t return this." + name + ";\n" + "}\n";
+		    method+= "public " + "List<String>" + " getOption" + LibUtils.firstLetterUpper(name) + "() {\n" + "\t return this.option" + name + ";\n" + "}\n";
+		} else {
+			method = "public " + LibUtils.changeToPrimitiveType(type) + " get" + LibUtils.firstLetterUpper(name) + "() {\n" + "\t return this." + name + ";\n" + "}\n";
+		}	
+		
+		
 		return method;
 	}
-	
+
 }
