7aaca7c43d115c22bd5b964a0a08d26b4e1dff31
joaquin <joaquin@jpereira>
2015-07-30 08:37:29 -0430

    Borrando imports no usados

--- src/ve/gob/cnti/helper/util/LibUtils.java
+++ src/ve/gob/cnti/helper/util/LibUtils.java
@@ -1,13 +1,9 @@
 package ve.gob.cnti.helper.util;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.Properties;
 
-import ve.gob.cnti.App;
 import ve.gob.cnti.helper.input.ReadFile;
 
 public class LibUtils {
