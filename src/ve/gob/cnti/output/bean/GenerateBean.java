bf5d6f609257cf8730bbba587b61be088d55f13e
jluis <jluis@cnti.gob.ve>
2016-03-08 11:53:30 -0430

    Cambios de compatibilidad con multiselect

--- src/ve/gob/cnti/output/bean/GenerateBean.java
+++ src/ve/gob/cnti/output/bean/GenerateBean.java
@@ -120,7 +120,7 @@ public class GenerateBean {
 		
 		if (isReadOnly) {
 			if (type.equals("List") && !name.matches("^_FILE_.*$")) {
-				return "@FieldIsActuacion\nprivate " + "List<String>" + " " + name + ";\n" + "private " + "List<String> " + name + "Option" + " = Arrays.asList(" + this.createMethodSelectValue(options) + ");\n";
+				return "@FieldIsActuacion\nprivate " + "String" + " " + name + ";\n" + "private " + "List<String> " + name + "Option" + " = Arrays.asList(" + this.createMethodSelectValue(options) + ");\n";
 			} else if (typeInput.equals("LISTBOX_SIMPLE") || typeInput.equals("RADIOBUTTON_GROUP") || typeInput.equals("CHECKBOX_GROUP")) {
 				return "@FieldIsActuacion\nprivate " + "String" + " " + name + ";\n" + "private " + "List<String> " + name + "Option" + " = Arrays.asList(" + this.createMethodSelectValue(options) + ");\n";
 			} else {
@@ -162,7 +162,7 @@ public class GenerateBean {
 		String method = null;
 
 		if (type.equals("List") && !name.matches("^_FILE_.*$")) {
-			method = "public void set" + LibUtils.firstLetterUpper(name) + "(" + "List<String>" + " " + name + " ) {\n" + "\t this." + name + " = " + name + ";\n" + "}\n";
+			method = "public void set" + LibUtils.firstLetterUpper(name) + "(" + "String" + " " + name + " ) {\n" + "\t this." + name + " = " + name + ";\n" + "}\n";
 			method += "public void set" + LibUtils.firstLetterUpper(name) + "Option" + "(" + "List<String> " + name + "Option" + " ) {\n" + "\t this." + name + "Option" + "= " + name + "Option" + ";\n" + "}\n";
 		} else if (typeInput.equals("LISTBOX_SIMPLE") || typeInput.equals("RADIOBUTTON_GROUP") || typeInput.equals("CHECKBOX_GROUP")) {
 			method = "public void set" + LibUtils.firstLetterUpper(name) + "(" + "String" + " " + name + " ) {\n" + "\t this." + name + " = " + name + ";\n" + "}\n";
@@ -177,7 +177,7 @@ public class GenerateBean {
 	public String createGetMethod(String name, String type, String typeInput) {
 		String method = null;
 		if (type.equals("List") && !name.matches("^_FILE_.*$")) {
-			method = "public " + "List<String>" + " get" + LibUtils.firstLetterUpper(name) + "() {\n" + "\t return this." + name + ";\n" + "}\n";
+			method = "public " + "String" + " get" + LibUtils.firstLetterUpper(name) + "() {\n" + "\t return this." + name + ";\n" + "}\n";
 			method += "public " + "List<String>" + " get" + LibUtils.firstLetterUpper(name) + "Option" + "() {\n" + "\t "
 					+ "return new Utils().convertlistStringToListStringUTF8(this." + name + "Option" + ");\n" + "}\n";
 		} else if (typeInput.equals("LISTBOX_SIMPLE") || typeInput.equals("RADIOBUTTON_GROUP") || typeInput.equals("CHECKBOX_GROUP")) {
